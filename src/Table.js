6626efeb7e3310bc8eb5083153ce38a49f3cc581
Bharath <bharathsurya021@gmail.com>
2022-11-13 14:12:18 +0530

    react basics

---
+++ src/Table.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Table = () => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Job</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Bharath</td>
+                    <td>Developer</td>
+                </tr>
+                <tr>
+                    <td>Surya</td>
+                    <td>UI/UX Designer</td>
+                </tr>
+                <tr>
+                    <td>Karthick</td>
+                    <td>Data Analyst</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export default Table
--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// //JSX
+// const heading = <h1 className='site-heading'>Hello, React !(JSX)</h1>
+
+// //No JSX
+// const heading1 = React.createElement('h1', { className: 'site-heading' }, 'Hello, React using no jsx')
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
